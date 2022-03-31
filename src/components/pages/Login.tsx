165f2d55cbc9c1a00c4b1000d661cdea3e540449
tomy-defi <98929210+tomy-defi@users.noreply.github.com>
2022-03-31 22:25:33 +0900

    Initial commit

---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,10 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import { memo, VFC } from "react";
+
+export const Login: VFC = memo(() => {
+  return (
+    <Flex>
+      <Box></Box>
+    </Flex>
+  );
+});
